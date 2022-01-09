af027f4168feddfb2cb4da3417bc33c285d0dc1a
cousim46 <wjdghldns24@naver.com>
2022-01-09 22:47:01 +0900

    jpa 영속성 컨텍스트

--- springboot-jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ springboot-jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -15,15 +15,25 @@ public class JpaMain {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         try {
+            //비 영속성
+            Member member = new Member();
+            member.setName("HelloJPA");
+            member.setId(100L);
+
+            //영속
+            System.out.println("==== BEFORE=====");
+            em.persist(member);
+            System.out.println("==== AFTER=====");
+
           //  Member member = em.find(Member.class, 1L);
-         List<Member> result =  em.createQuery("select m from Member as m",Member.class)
+      /*   List<Member> result =  em.createQuery("select m from Member as m",Member.class)
                  .setFirstResult(1)
                  .setMaxResults(10)
                  .getResultList();
             for (Member member : result) {
                 System.out.println("member.getName() = " + member.getName());
             }
-
+*/
          transaction.commit();
         }catch(Exception e) {
             transaction.rollback();;
