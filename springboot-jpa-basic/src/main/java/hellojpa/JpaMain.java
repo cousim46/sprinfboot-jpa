23f2a7bfa6247d15ea2799be2fa332f2c8122100
cousim46 <wjdghldns24@naver.com>
2022-01-15 18:41:43 +0900

    임베디드 타입(Embeddable,Embedded), 값 타입

--- springboot-jpa-basic/src/main/java/hellojpa/JpaMain.java
+++ springboot-jpa-basic/src/main/java/hellojpa/JpaMain.java
@@ -16,20 +16,17 @@ public class JpaMain {
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
         try {
-            Child child1 = new Child();
-            Child child2 = new Child();
-            Parent parent = new Parent();
-            parent.addChild(child1);
-            parent.addChild(child2);
+            Address address = new Address("city", "street", "zipcode");
+            Member member1 = new Member();
+            member1.setUsername("hello");
+            member1.setHomeAddress(address);
+            em.persist(member1);
 
-            em.persist(parent);
-           /* em.persist(child1);
-            em.persist(child2);
-*/
-            em.flush();
-            em.clear();
-            Parent parent1 = em.find(Parent.class, parent.getId());
-            parent1.getChildren().remove(0);
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member1.setHomeAddress(newAddress);
+
+
+
             transaction.commit();
         } catch (Exception e) {
             transaction.rollback();
@@ -42,10 +39,10 @@ public class JpaMain {
 
 
     }
-    private static void printMemberAndTeam(Member member) {
+   /* private static void printMemberAndTeam(Member member) {
         String username = member.getUsername();
         System.out.println("username = " + username);
         Team team = member.getTeam();
         System.out.println("team.getName() = " + team.getName());
-    }
+    }*/
 }
--- springboot-jpa-basic/src/main/java/hellojpa/Member.java
+++ springboot-jpa-basic/src/main/java/hellojpa/Member.java
@@ -12,55 +12,24 @@ import java.util.List;
         name="MEMBER_SEQ_GENERATOR",
         table="MY_SEQUENCES"
         ,pkColumnValue = "MEMBER_SEQ", allocationSize = 1)*/
+public class Member {
 
-public class Member extends BaseEntity{
 
-
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     @Column(name = "MEMBER_ID")
     private Long id;
 
     @Column(name = "USERNAME")
     private String username;
 
-    /*@OneToOne
-    @JoinColumn(name = "LOCKER_ID")
-    private Locker locker;*/
+    //기간
+    @Embedded
+    private Period workPeriod;
 
-   @OneToMany(mappedBy = "member")
-   private List<MemberProduct> member= new ArrayList<>();
-
-    @ManyToOne(fetch = FetchType.EAGER)
-    @JoinColumn(name="TEAM_ID")
-    private Team team;
-
-   /* public Locker getLocker() {
-        return locker;
-    }
-
-    public void setLocker(Locker locker) {
-        this.locker = locker;
-    }*/
-
-    public List<MemberProduct> getMember() {
-        return member;
-    }
-
-    public void setMember(List<MemberProduct> member) {
-        this.member = member;
-    }
-
-    public Team getTeam() {
-        return team;
-    }
-
-    public void setTeam(Team team) {
-        this.team = team;
-    }
-
-    /* @Column(name="TEAM_ID")
-    private Long teamId;
-*/
+    // 주소
+    @Embedded
+   private Address homeAddress;
 
     public Long getId() {
         return id;
@@ -78,119 +47,19 @@ public class Member extends BaseEntity{
         this.username = username;
     }
 
-
-
-    /* @Id
-        // @GeneratedValue(strategy = GenerationType.TABLE, generator = "MEMBER_SEQ_GENERATOR")
-
-         @GeneratedValue(strategy = GenerationType.IDENTITY)
-         private Long id;
-
-         @Column(name = "name", nullable = false)
-         private String username;
-
-         public Long getId() {
-             return id;
-         }
-
-         public void setId(Long id) {
-             this.id = id;
-         }
-
-         public String getUsername() {
-             return username;
-         }
-
-         public void setUsername(String username) {
-             this.username = username;
-         }
-     */
-    /*
-
-            private Integer age;
-
-            @Enumerated(EnumType.STRING)
-            private RoleType roleType;
-
-            @Temporal(TemporalType.TIMESTAMP)
-            private Date createdDate;
-
-            @Temporal(TemporalType.TIMESTAMP)
-            private Date lastModifiedDate;
-
-            private LocalDate testLocalDate;
-            private LocalDateTime testLocalDateTime;
-
-            @Lob
-            private String description;
-
-
-            @Transient
-            private int temp;*/
-    public Member() {
+    public Period getWorkPeriod() {
+        return workPeriod;
     }
 
-    /*public Long getId() {
-        return id;
+    public void setWorkPeriod(Period workPeriod) {
+        this.workPeriod = workPeriod;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public Address getHomeAddress() {
+        return homeAddress;
     }
 
-    public String getUsername() {
-        return username;
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
     }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public RoleType getRoleType() {
-        return roleType;
-    }
-
-    public void setRoleType(RoleType roleType) {
-        this.roleType = roleType;
-    }
-
-    public Date getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Date createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public Date getLastModifiedDate() {
-        return lastModifiedDate;
-    }
-
-    public void setLastModifiedDate(Date lastModifiedDate) {
-        this.lastModifiedDate = lastModifiedDate;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
-    }*/
 }
