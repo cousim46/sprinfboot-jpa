81a29cae9f9e345b319016c9e9de1dedb7312171
cousim46 <wjdghldns24@naver.com>
2022-01-16 21:03:53 +0900

    JPQL : coalesce, nullif

--- JPQL/src/main/java/jpql/JpaMain.java
+++ JPQL/src/main/java/jpql/JpaMain.java
@@ -19,19 +19,14 @@ public class JpaMain {
             em.persist(team);
 
             Member member = new Member();
-            member.setUsername("teamA");
+            member.setUsername("관리자");
             member.setAge(10);
             member.setTeam(team);
             em.persist(member);
             em.flush();
             em.clear();
 
-            String query = "select " +
-                                "case when m.age <= 10 then '학생 요금'" +
-                                "     when m.age >= 60 then '경로 요금'"+
-                                "     else '일반요금'"+
-                    "end "+
-                    "from Member m";
+            String query ="select nullif(m.username,'관리자') from Member m ";
             List<String> resultList = em.createQuery(query, String.class).getResultList();
             for (String s : resultList) {
                 System.out.println("s = " + s);
