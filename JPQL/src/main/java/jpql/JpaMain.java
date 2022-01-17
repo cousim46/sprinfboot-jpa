145cbed963627658a5f4d0d08e0d20257bbf0755
cousim46 <wjdghldns24@naver.com>
2022-01-17 19:23:35 +0900

    fetch join

--- JPQL/src/main/java/jpql/JpaMain.java
+++ JPQL/src/main/java/jpql/JpaMain.java
@@ -13,25 +13,36 @@ public class JpaMain {
         transaction.begin();
         try {
 
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
 
-            Member member = new Member();
-            member.setUsername("관리자");
-            member.setAge(10);
-            member.setTeam(team);
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
+
             em.flush();
             em.clear();
 
-            String query ="select nullif(m.username,'관리자') from Member m ";
-            List<String> resultList = em.createQuery(query, String.class).getResultList();
-            for (String s : resultList) {
-                System.out.println("s = " + s);
-            }
 
+            String query = "select t from Team t join fetch t.members m";
+            List<Team> result = em.createQuery(query, Team.class).getResultList();
+            System.out.println("result = " + result.size());
 
             transaction.commit();
         } catch (Exception e) {
