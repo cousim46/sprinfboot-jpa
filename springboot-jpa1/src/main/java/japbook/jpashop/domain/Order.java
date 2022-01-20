318bf5f178321e33b26f2a501965589564de67df
cousim46 <wjdghldns24@naver.com>
2022-01-20 17:36:30 +0900

    연관관계 편의 메서드, fetch타입 => lazy(OneToOne, ManyToOne)

--- springboot-jpa1/src/main/java/japbook/jpashop/domain/Order.java
+++ springboot-jpa1/src/main/java/japbook/jpashop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItem = new ArrayList<>();
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
@@ -33,4 +33,21 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus deliverStatus;
+
+    // 연관관계 편의 메서드 => 양방향일때 사용하면 좋다
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDellivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
 }
