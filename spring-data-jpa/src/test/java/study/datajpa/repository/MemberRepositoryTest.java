a074ef6d98c67d89feaa536fc5ee97f8b7228a34
cousim46 <wjdghldns24@naver.com>
2022-01-30 00:53:29 +0900

    @EntityGraph를 이용한 fetch 조인

--- spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -228,4 +228,30 @@ public class MemberRepositoryTest {
 
     }
 
+    @Test
+    public void findMemberLazy() throws Exception {
+        // given
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 10, teamB);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        em.flush();
+        em.clear();
+        // when
+       // List<Member> members = memberRepository.findAll();
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+       // List<Member> members = memberRepository.findMemberFetchJoin();
+        System.out.println("====================================");
+
+        for (Member member : members) {
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member.team = " + member.getTeam().getName());
+        }
+        // then
+    }
+
 }
