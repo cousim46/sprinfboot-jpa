2ea2f1890b7c22e8974572d03c646886d43b5a46
cousim46 <wjdghldns24@naver.com>
2022-01-30 00:22:58 +0900

    벌크성 수정

--- spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ spring-data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -14,6 +14,8 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -27,6 +29,8 @@ public class MemberRepositoryTest {
     MemberRepository memberRepository;
     @Autowired
     TeamRepository teamRepository;
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() throws Exception {
@@ -202,6 +206,25 @@ public class MemberRepositoryTest {
        Assertions.assertThat(page.hasNext()).isTrue();
 
 
+
+    }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        memberRepository.save(new Member("member1",10));
+        memberRepository.save(new Member("member2",19));
+        memberRepository.save(new Member("member3",20));
+        memberRepository.save(new Member("member4",21));
+        memberRepository.save(new Member("member5",40));
+
+        // when
+        int resultCount = memberRepository.bulkAgePlus(20);
+
+       // em.clear();
+        List<Member> result = memberRepository.findByUsername("member5");
+        Member member5 = result.get(0);
+        System.out.println("member5 = " + member5);
+        Assertions.assertThat(resultCount).isEqualTo(3);
 
     }
 
