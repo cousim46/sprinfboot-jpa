e7e160115ae20193775422a6a51d436543102ff2
cousim46 <wjdghldns24@naver.com>
2022-02-04 17:35:34 +0900

    페이징 처리

--- spring-data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ spring-data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -20,14 +24,25 @@ public class MemberController {
         Member member = memberRepository.findById(id).get();
         return member.getUsername();
     }
+
     @GetMapping("/members2/{id}")
     public String findMember2(@PathVariable("id") Member member) {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size=5, sort="id") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+
+        return map;
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i, i));
+        }
     }
 
 }
